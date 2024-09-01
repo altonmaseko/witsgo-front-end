b34f375194f943b0d86d0f539c96c798730819df
Prashant Kessa <prashant.kessa@gmail.com>
2024-09-01 19:16:23 +0200

    html and css for navigate screen. maps not incorperated yet

---
+++ Navigate Screen/nav.js
@@ -0,0 +1,7 @@
+const searchBtn = document.getElementById("searchBtn");
+const navMeBtn = document.getElementById("navBtn");
+const profileBtn = document.getElementById("profileBtn");
+
+profileBtn.addEventListener("click", function() {
+    window.location.href = "Profile%20Page/profile.html";
+});
