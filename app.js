def00aa2c2e1de4b4addb6dd46c956f903193588
Alton Maseko <altonmaseko1000@gmail.com>
2024-09-10 11:02:27 +0200

    port

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
 // create basic server to server the files in the root directory
 const express = require("express")
+require('dotenv').config()
 
 const app = express()
 
 app.use(express.static("./"))
 
-app.listen(5000, () => {
-    console.log("Server is running on port 5000")
+const PORT = process.env.PORT;
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}...`)
 })
 
 
