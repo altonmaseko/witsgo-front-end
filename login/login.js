4ea615f649f1f4e13c1ad6e8133e1c60d36d1f7a
Alton Maseko <altonmaseko1000@gmail.com>
2024-09-17 02:03:16 +0200

    wiped

--- login/login.js
+++
@@ -1,8 +0,0 @@
-const loginWithGoogleButton = document.getElementById('login-with-google-button');
-
-import { clientUrl, serverUrl } from "../constants.js";
-
-loginWithGoogleButton.addEventListener('click', () => {
-    const homepage = encodeURIComponent(`${clientUrl}/Real Time Tracking/real-time-tracking.html`);
-    window.location.href = `${serverUrl}/auth/google?redirect=${homepage}`;
-});
