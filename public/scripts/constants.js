33aa6f4cefb0095a2f5ef01c850336be8cc88046
Alton Maseko <altonmaseko1000@gmail.com>
2024-09-17 10:19:29 +0200

    testing ignore

--- public/scripts/constants.js
+++ public/scripts/constants.js
@@ -1,7 +1,7 @@
-// export const serverUrl = 'http://localhost:3000';
-// export const clientUrl = 'http://localhost:5000';
+export const serverUrl = 'http://localhost:3000';
+export const clientUrl = 'http://localhost:5000';
 
 // Production
-export const serverUrl = 'https://witsgobackend.azurewebsites.net';
-export const clientUrl = 'https://witsgo.azurewebsites.net';
+// export const serverUrl = 'https://witsgobackend.azurewebsites.net';
+// export const clientUrl = 'https://witsgo.azurewebsites.net';
 
