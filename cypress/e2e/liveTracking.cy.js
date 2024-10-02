567b19c3ef7e049a64f33a72f1774a4bb94fa01e
lucianIrsigler <l.irsigler64@gmail.com>
2024-10-02 19:50:34 +0200

    yes

---
+++ cypress/e2e/liveTracking.cy.js
@@ -0,0 +1,62 @@
+describe('Live tracking', () => {
+    beforeEach(() => {
+      cy.viewport(360, 800);
+      // Setting up localStorage before visiting the page
+      cy.window().then((window) => {
+        window.localStorage.setItem('role', 'student'); // Example role
+      });
+      cy.intercept('GET', `${Cypress.env('serverUrl')}/verifylogin`, {
+        statusCode: 200,
+        body: {
+            isLoggedIn: true,
+            user: {
+                user: {
+                    firstName: 'John',
+                    lastName: 'Doe',
+                    picture: 'https://example.com/profile.jpg',
+                    onWheelChair: false,
+                    email: 'john.doe@example.com',
+                    role: 'student',
+                    _id: '12345'
+                }
+            }
+        }
+    }).as('verifyLogin');
+    
+      cy.visit('http://localhost:5000/realTimeTracking.html',{
+        onBeforeLoad(win) {
+          cy.stub(win.navigator.geolocation, 'getCurrentPosition').callsFake((success, error) => {
+            success({
+              coords: {
+                latitude: -26.1913581,
+                longitude: 28.0267878,
+              },
+            });
+          });
+        },
+        }); 
+    });
+  
+    it('Checking elements', () => {
+      // Retrieve the role from localStorage and check its value
+      cy.window().then((window) => {
+        const role = window.localStorage.getItem('role');
+        expect(role).to.equal('student'); // Verify that the role is set correctly
+      });
+
+      cy.get(".check-and-vehicle-container").should("be.visible");
+
+      cy.get(".check-and-vehicle").eq(0).should("be.visible")
+      cy.get(".vehicle").eq(0).contains("Wits Bus")
+
+      cy.get(".check-and-vehicle").eq(1).should("be.visible")
+      cy.get(".vehicle").eq(1).contains("Campus Control Shuttle")
+
+      cy.get(".check-and-vehicle").eq(2).should("be.visible")
+      cy.get(".vehicle").eq(2).contains("Campus Security")
+
+    });
+
+
+});
+  
---
+++ cypress/e2e/siteNavigation.cy.js
@@ -0,0 +1,74 @@
+describe('Navbar Test', () => {
+  beforeEach(() => {
+    cy.viewport(360, 800);
+    cy.intercept('GET', `${Cypress.env('serverUrl')}/verifylogin`, {
+      statusCode: 200,
+      body: {
+          isLoggedIn: true,
+          user: {
+              user: {
+                  firstName: 'John',
+                  lastName: 'Doe',
+                  picture: 'https://example.com/profile.jpg',
+                  onWheelChair: false,
+                  email: 'john.doe@example.com',
+                  role: 'student',
+                  _id: '12345'
+              }
+          }
+      }
+  }).as('verifyLogin');
+  
+    cy.window().then((window) => {
+      window.localStorage.setItem('role', 'student'); // Example role
+    });
+
+    cy.visit('http://localhost:5000/navigation.html'); // Replace with your actual page URL
+  });
+
+  it('Should display the navbar with correct links and text', () => {
+    cy.wait("@verifyLogin")
+    
+    // Check if the navbar section exists
+    cy.get('.navbar').should('exist');
+
+    // Check for the second navigation link (Renting)
+    cy.get('.navbar a').eq(1)
+      .should('have.attr', 'href', 'rental')
+      .find('img')
+      .should('have.attr', 'src', 'icons/rent-grey.png')
+      .next('.text')
+      .should('contain', 'Renting');
+
+    // Check for the third navigation link (Schedule)
+    cy.get('.navbar a').eq(2)
+      .should('have.attr', 'href', 'buses')
+      .find('img')
+      .should('have.attr', 'src', 'icons/schedule-grey.png')
+      .next('.text')
+      .should('contain', 'Schedule');
+
+    // Check for the fourth navigation link (Tracking)
+    cy.get('.navbar a').eq(3)
+      .should('have.attr', 'href', 'tracktransport')
+      .find('img')
+      .should('have.attr', 'src', 'icons/tracking-grey.png')
+      .next('.text')
+      .should('contain', 'Tracking');
+  });
+
+  it('Should navigate to correct pages when navbar links are clicked', () => {
+    cy.get('.navbar a').eq(0).click();
+    cy.url().should('include', '/navigation');
+
+    cy.get('.navbar a').eq(1).click();
+    cy.url().should('include', '/rental');
+
+    cy.get('.navbar a').eq(2).click();
+    cy.url().should('include', '/buses');
+
+    cy.get('.navbar a').eq(3).click();
+    cy.url().should('include', '/tracktransport');
+  });
+
+});
