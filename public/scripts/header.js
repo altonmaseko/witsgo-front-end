5121b35e8c746e10291db8e09351a722cfdbb96b
Alton Maseko <altonmaseko1000@gmail.com>
2024-10-02 19:35:54 +0200

    updating

--- public/scripts/header.js
+++ public/scripts/header.js
@@ -1,16 +1,16 @@
 const dropdownButton = document.getElementById('dropdown-button');
 const dropdownMenu = document.getElementById('dropdown-menu');
 
-    dropdownButton.addEventListener('click', function () {
-        if (dropdownMenu.style.display === 'block') {
-            dropdownMenu.style.display = 'none'; 
-        } else {
-            dropdownMenu.style.display = 'block';  
-        }
-    });
+dropdownButton.addEventListener('click', function () {
+    if (dropdownMenu.style.display === 'block') {
+        dropdownMenu.style.display = 'none';
+    } else {
+        dropdownMenu.style.display = 'block';
+    }
+});
 
-    window.addEventListener('click', function(event) {
-        if (!event.target.closest('.dropdown') && dropdownMenu.style.display === 'block') {
-            dropdownMenu.style.display = 'none';  
-        }
-    });
+window.addEventListener('click', function (event) {
+    if (!event.target.closest('.dropdown') && dropdownMenu.style.display === 'block') {
+        dropdownMenu.style.display = 'none';
+    }
+});
